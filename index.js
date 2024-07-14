6857261c210a7fa2ebe1d7e35356367dbc5f79f4
Gurbanov Javid <melloalter@gmail.com>
2024-07-14 22:17:55 +0400

    JS 2 WORK

---
+++ index.js
@@ -0,0 +1,141 @@
+// let a = +prompt('Возраст')
+// if( a>=0 && a <= 2){
+//     alert('Ребёнок')
+// }
+// else if(a >=12 && a < 18){
+//     alert('Подросток')
+// }
+// else if( a >= 18 && a <= 60){
+//     alert('Взрослый')
+// }
+// else{
+//     alert('Пенсионер')
+// }
+
+
+// let a = prompt('Число')
+// switch(a){
+//     case '0':
+//         alert(')')
+//         break;
+//         case '1':
+//             alert('!')
+//             break;
+//             case '2':
+//                 alert('@')
+//                 break;
+//                 case '3':
+//                     alert('#')
+//                     break;
+//                     case '4':
+//                         alert('$')
+//                         break;
+//                         case '5':
+//                             alert('%')
+//                             break;
+//                             case '6':
+//                                 alert('^')
+//                                 break;
+//                                 case '7':
+//                                     alert('&')
+//                                     break;
+//                                     case '8':
+//                                         alert('*')
+//                                         break;
+//                                         case '9':
+//                                             alert('(')
+//                                             break;
+//                                             default:
+//                                                 alert('Нет символов')
+// }
+
+
+// let num = prompt('Введите 3-х значное число')
+// if(num[0] == num1] || num[0]== num[2] || num[1]==num[2]){
+//     alert('Есть повторение чисел')
+// }
+// else{
+//     alert('Повторений нет')
+// }
+
+// let num = prompt('Год')
+// if((num % 400 == 0 || num % 4 == 0 ) && (num % 100 != 0)){
+//     alert('Высокосный')
+// }
+// else{
+//     alert('Невысокостный')
+// }
+
+
+// let num = +prompt('Пяти значное число')
+// if(num[0] == num[4] && num[1] == num[3]){
+//     alert('Число полиндром')
+// }
+// else{
+//     alert('Не полиндром')
+// }
+
+// let usdt = +prompt('Кол-во денег')
+// let valute = prompt('AZN, EUR , UAN')
+// if(valute == 'AZN'){
+//     alert(usdt*1.7)
+// }
+// else if(valute == 'EUR'){
+//     alert(usdt*0.92)
+// }
+// else if(valute == 'UAN'){
+//     alert(usdt*7.25)
+// }
+// else{
+//     alert('Такой купюры нет')
+// }
+
+// let num = +prompt('Сумма покупки')
+// if(num >= 200 && num < 300){
+//     alert(num - (num*0.03))
+// }
+// else if(num >= 300 && num < 500){
+//     alert(num - (num*0.05))
+// }
+// else if(num >= 500){
+//     alert(num - (num*0.07))
+// }
+// else{
+//     alert('Скидки нет')
+// }
+
+// let num = +prompt('Окружность')
+// let num1 = +prompt('Периметр')
+// let diametr = num / 3.14;
+// let kvadrat = num1 /4;
+// if(diametr <= kvadrat){
+//     alert('Поместиться')
+// }
+// else{
+//     alert('Не помещается')
+// }
+
+
+// let num1 = prompt('Сделал уроки Да,Нет,Не всё задание' )
+// let num2 = prompt('2 умножить на 4')
+// let num3 = prompt('Хочу в отпуск Да,Нет,Очень')
+// if(num1 == 'Да'){
+//     num1 = 2
+// }
+// else {
+//     num1 = 0
+// }
+
+// if (num2 == 8)
+//     num2 = 2
+// else {
+//     num2 = 0
+// }
+
+// if (num3 == 'Очень')
+//     num3 = 2
+// else {
+//     num3 = 0
+// }
+
+// alert(num1 + num2 + num3);
